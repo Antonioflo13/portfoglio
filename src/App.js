e200183cc5faaf3067dac1962afbe3fe909a1511
Antonioflo13 <floreantonio@hotmail.it>
2021-10-07 15:27:45 +0200

    add: section projects

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import 'bootstrap';
 import './App.scss';
 import Navbar from '../src/component/Navbar.js';
 import Main from '../src/component/Main.js';
+//Projects Image 
+import Boolzap from "./img/Boolzap.png";
 
 
 function App() {
@@ -12,8 +14,24 @@ function App() {
     ['Home'],
     ['About-me'],
     ['Tecnologies'],
+    ['Projects'],
     ['Certifications']
   ]);
+
+  const projects = useState([
+    {
+      img: {Boolzap},
+      description: "Web app realizzata con HTML, CSS e Vue.js. Simulazione di invio di messaggi con risposta automatica, ricerca dei contatti dinamica, cancellazione messaggi, gestione dark mode.",
+      gitHubRepo : "https://github.com/Antonioflo13/vue-boolzapp.git",
+      siteLink: "https://vue-boolzap.netlify.app/"
+    },
+    {
+      img: {Boolzap},
+      description: "Web app realizzata con HTML, CSS e Vue.js. Simulazione di invio di messaggi con risposta automatica, ricerca dei contatti dinamica, cancellazione messaggi, gestione dark mode.",
+      gitHubRepo : "https://github.com/Antonioflo13/vue-boolzapp.git",
+      siteLink: ""
+    },
+  ])
 
   return (
     <div className="App">
@@ -21,7 +39,7 @@ function App() {
         <Navbar links={links}/>
       </header>
       <main>
-      <Main />
+      <Main projects={projects}/>
       </main>
     </div>
   );
--- src/component/Main.js
+++ src/component/Main.js
@@ -1,4 +1,6 @@
-function Main () {
+import Boolzap from "../img/Boolzap.png";
+
+function Main (props) {
     return (
        <section id="ms_main">
            <div id="icon-profile" className="d-flex align-self-end justify-content-end">
@@ -14,24 +16,24 @@ function Main () {
            <article id="Home" className="container" data-aos="fade">
                <div className="d-flex flex-column justify-content-center align-items-center ms_box">
                     <h1>Hello, I am Antonio.</h1>
-                    <p>Jr. Full Stack Developer</p>
+                    <p>Jr. Full Stack Web Developer</p>
                </div>
            </article>
             <article className="container" data-aos="fade-up">
                 <div id="About-me" className="d-flex flex-column justify-content-center align-items-center ms_box">
                    <h2>About Me</h2>
-                   <div id="description" className="w-50">
+                   <div id="description" className="w-75">
                         <p>
                             Appassionato di informatica fin da piccolo, cerco di migliorarmi ogni giorno di più. Dopo un lungo periodo ricco di esperienze e soddisfazioni torno sui miei passi per far diventare la mia passione il mio lavoro.
                         </p>
                         <p>
-                            Da Marzo 2021 ho iniziato a frequentare un master presso la Tech School <a href="https://www.boolean.careers/corso/full-time?utm_source=google&utm_medium=cpc&utm_content=corso&gclid=CjwKCAjw4qCKBhAVEiwAkTYsPLasFS7UJS-Q10Zb8esM-BkDdmlNtjfMYCQ0r_9pbq0UXjdZos9_rxoCPnwQAvD_BwE">Boolean Careers</a> ; per 6 mesi / 700 ore di corso ho affrontato diverse sfide, mi hanno consentito di avere la giusta preparazione per imparare ogni giorno nuove tecnologie.
+                            Da Marzo 2021 ho iniziato a frequentare un master presso la Tech Accademy <a href="https://www.boolean.careers/corso/full-time?utm_source=google&utm_medium=cpc&utm_content=corso&gclid=CjwKCAjw4qCKBhAVEiwAkTYsPLasFS7UJS-Q10Zb8esM-BkDdmlNtjfMYCQ0r_9pbq0UXjdZos9_rxoCPnwQAvD_BwE">Boolean Careers</a> ; per 6 mesi / 700 ore di corso ho affrontato diverse sfide, mi hanno consentito di avere la giusta preparazione per imparare ogni giorno nuove tecnologie.
                         </p>
                    </div>
                 </div>
             </article>
             <article className="container">
-            <div id="Tecnologies" className="d-flex justify-content-center">
+            <div id="Tecnologies" className="d-flex justify-content-center align-items-center">
                     <ul className="list-inline ms_box d-flex flex-wrap align-content-center">
                         <li className="list-inline-item"  data-aos="zoom-in">
                             <a href="#"><i className="fab fa-html5 icon"></i></a>
@@ -70,6 +72,18 @@ function Main () {
                             <a href="#"><i className="fab fa-node-js icon"></i></a>
                         </li>
                     </ul>
+                </div>
+            </article>
+            <article className="container">
+                <div id="Projects" className="d-flex flex-column align-items-center justify-content-center ms_box">
+                    <h2 data-aos="fade-up">Projects</h2>
+                    <div className="ms_card  d-flex flex-column align-items-center justify-content-center">
+                        <h3>Boolzap</h3>
+                        <img src={Boolzap} alt="Boolzap" />
+                        <p className="w-75">Web app realizzata con HTML, CSS e Vue.js. Simulazione di invio di messaggi con risposta automatica, ricerca dei contatti dinamica, cancellazione messaggi, gestione dark mode.</p>
+                        <button className="btn-sm"><a href="https://github.com/Antonioflo13/vue-boolzapp.git" target="_blank" rel="noreferrer"><i className="fab fa-github icon"></i> Repository</a></button>
+                        <button className="btn btn-sm"><a href="https://vue-boolzap.netlify.app/" target="_blank" rel="noreferrer"><i class="fas fa-link"></i> Link</a></button>
+                    </div>
                 </div>
             </article>
             <article className="container">
